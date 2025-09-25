aef388d622b139dd646bbc39f4e84977755c90a5
Brent Baccala <cosine@freesoft.org>
2025-09-24 22:21:57 -0400

    bug fix in posInL110 and posInL110Ring
    The ordering goes degree, length, leading monomial.  The leading monomial should be checked
    only when the lengths are equal, not less than or equal.

--- kernel/GBEngine/kutil.cc
+++ kernel/GBEngine/kutil.cc
@@ -5817,7 +5817,7 @@ int posInL110 (const LSet set, const int length,
 
   if ((op > o)
   || ((op == o) && (set[length].length >p->length))
-  || ((op == o) && (set[length].length <= p->length)
+  || ((op == o) && (set[length].length == p->length)
      && (pLmCmp(set[length].p,p->p) != cmp_int)))
     return length+1;
   int i;
@@ -5830,7 +5830,7 @@ int posInL110 (const LSet set, const int length,
       op = set[an].GetpFDeg();
       if ((op > o)
       || ((op == o) && (set[an].length >p->length))
-      || ((op == o) && (set[an].length <=p->length)
+      || ((op == o) && (set[an].length == p->length)
          && (pLmCmp(set[an].p,p->p) != cmp_int)))
         return en;
       return an;
@@ -5839,7 +5839,7 @@ int posInL110 (const LSet set, const int length,
     op = set[i].GetpFDeg();
     if ((op > o)
     || ((op == o) && (set[i].length > p->length))
-    || ((op == o) && (set[i].length <= p->length)
+    || ((op == o) && (set[i].length == p->length)
        && (pLmCmp(set[i].p,p->p) != cmp_int)))
       an=i;
     else
@@ -5857,7 +5857,7 @@ int posInL110Ring (const LSet set, const int length,
 
   if ((op > o)
   || ((op == o) && (set[length].length >p->length))
-  || ((op == o) && (set[length].length <= p->length)
+  || ((op == o) && (set[length].length == p->length)
      && (pLtCmpOrdSgnDiffM(set[length].p,p->p))))
     return length+1;
   int i;
@@ -5870,7 +5870,7 @@ int posInL110Ring (const LSet set, const int length,
       op = set[an].GetpFDeg();
       if ((op > o)
       || ((op == o) && (set[an].length >p->length))
-      || ((op == o) && (set[an].length <=p->length)
+      || ((op == o) && (set[an].length == p->length)
          && (pLtCmpOrdSgnDiffM(set[an].p,p->p))))
         return en;
       return an;
@@ -5879,7 +5879,7 @@ int posInL110Ring (const LSet set, const int length,
     op = set[i].GetpFDeg();
     if ((op > o)
     || ((op == o) && (set[i].length > p->length))
-    || ((op == o) && (set[i].length <= p->length)
+    || ((op == o) && (set[i].length == p->length)
        && (pLtCmpOrdSgnDiffM(set[i].p,p->p))))
       an=i;
     else
